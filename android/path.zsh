2e76723f91b36e7e20fe62ced2fde7aec2865a43
Plastix <aidanpieper@gmail.com>
2016-11-29 08:29:14 -0500

    Add Android SDK to path

---
+++ android/path.zsh
@@ -0,0 +1,5 @@
+# Default Android SDK installation by Android Studio
+export ANDROID_HOME=$HOME/Library/Android/sdk/
+
+# Make Android utilities available from the command line (i.e. adb)
+export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
