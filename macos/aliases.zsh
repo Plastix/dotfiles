89f0254ae63a4dbac23cfe2b10018ad0c2116971
Plastix <aidanpieper@gmail.com>
2016-12-01 08:59:23 -0500

    Add some custom aliases

---
+++ macos/aliases.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Show/hide hidden files in Finder
+alias showdotfiles="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hidedotfiles="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# Lock the screen (when going AFK)
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
---
+++ system/aliases.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Easier navigation: .., ..., ...., .....,
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Shortcuts
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+
+# Enable aliases to be sudo’ed
+alias sudo='sudo '
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
+
+alias reload="source ~/.zshrc && echo 'Shell config reloaded from ~/.zshrc'"
